b2d86c6306cf8f4ba524a306e91167b67173fa46
Bárbara Souza <ibarbarasouza@icloud.com>
2021-10-18 20:50:35 -0300

    snapCrackle

--- src/snapCrackle.js
+++ src/snapCrackle.js
@@ -1,1 +1,25 @@
-// Seu código vai aqui
+function snapCrackle (maxValue) {
+    let meuArray = [];
+    
+    for (let contador = 1; contador <= maxValue; contador++){
+    
+        if (contador % 5 === 0 && contador % 2 === 1){
+            meuArray.push('SnapCrackle')
+    
+        }else if (contador % 5 === 0){
+            meuArray.push('Crackle')
+    
+        }else if (contador % 2 === 1){
+            meuArray.push('Snap') 
+    
+        }else if (contador % 5 !== 0 && contador % 2 !== 1){
+            meuArray.push(contador)
+        }
+    }
+    return meuArray.join(', ')
+    }
+    console.log(snapCrackle(15))
+
+
+
+ 
